1ce86645a17196c8bc47438a156fca6d599f43fe
Sneha Roseline Augustine <146040464+sneha-roseline@users.noreply.github.com>
2023-11-21 18:28:20 -0500

    news_automation_dag.py

--- news_automation_airflow.py
+++ news_automation_airflow.py
@@ -11,17 +11,30 @@ from airflow.providers.amazon.aws.sensors.glue import GlueJobSensor
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Define the 'args' variable with appropriate configuration settings
+args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 8, 1),
+    'email': ['myemail@domain.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(seconds=15),
+}
 
 dag = DAG(
-    dag_id="news_automation_dag", default_args=args, schedule_interval=None
+    dag_id="news_automation_dag",
+    default_args=args,
+    schedule_interval=None
 )
 
 def glue_job_s3_dests3_transfer(job_name, **kwargs):
     session = AwsGenericHook(aws_conn_id='aws_s3_conn')
-      
+
     # Get a client in the same region as the Glue job
     boto3_session = session.get_session(region_name='us-east-2')
-    
+
     # Trigger the job using its name
     client = boto3_session.client('glue')
     client.start_job_run(
@@ -33,56 +46,44 @@ def get_run_id():
     session = AwsGenericHook(aws_conn_id='aws_s3_conn')
     boto3_session = session.get_session(region_name='us-east-2')
     glue_client = boto3_session.client('glue')
-    response = glue_client.get_job_runs(JobName="glue_etl")
+    response = glue_client.get_job_runs(JobName="newsapi-etl-job")
     job_run_id = response["JobRuns"][0]["Id"]
-    return job_run_id 
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 8, 1),
-    'email': ['myemail@domain.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 2,
-    'retry_delay': timedelta(seconds=15)
-}
-
+    return job_run_id
 
 with dag:
 
-	extract_news_info = PythonOperator(
-	task_id='extract_news_info',
-	python_callable=runner,
-	dag=dag, 
-	)
-		
-	move_file_to_s3 = BashOperator(
-	task_id="move_file_to_s3",
-	bash_command='aws s3 mv {{ ti.xcom_pull("extract_news_info")}}  s3://landings3bucket-sneha',
-	)
+    extract_news_info = PythonOperator(
+        task_id='extract_news_info',
+        python_callable=runner,
+        dag=dag,
+    )
+
+    move_file_to_s3 = BashOperator(
+        task_id="move_file_to_s3",
+        bash_command='aws s3 mv {{ ti.xcom_pull("extract_news_info") }}  s3://landings3bucket-sneha',
+    )
 
     glue_job_trigger = PythonOperator(
-    task_id='tsk_glue_job_trigger',
-    python_callable=glue_job_s3_dests3_transfer,
-    op_kwargs={
-        'job_name': 'glue_etl'
-    },
+        task_id='tsk_glue_job_trigger',
+        python_callable=glue_job_s3_dests3_transfer,
+        op_kwargs={
+            'job_name': 'newsapi-etl-job'
+        },
     )
 
     grab_glue_job_run_id = PythonOperator(
-    task_id='tsk_grab_glue_job_run_id',
-    python_callable=get_run_id,
+        task_id='tsk_grab_glue_job_run_id',
+        python_callable=get_run_id,
     )
 
     is_glue_job_finish_running = GlueJobSensor(
-    task_id="tsk_is_glue_job_finish_running",      
-    job_name='glue_etl',
-    run_id='{{task_instance.xcom_pull("tsk_grab_glue_job_run_id")}}',
-    verbose=True,  # prints glue job logs in airflow logs
-    aws_conn_id='aws_s3_conn',
-    poke_interval=60,
-    timeout=3600,
+        task_id="tsk_is_glue_job_finish_running",
+        job_name='newsapi-etl-job',
+        run_id='{{task_instance.xcom_pull("tsk_grab_glue_job_run_id")}}',
+        verbose=True,  # prints glue job logs in airflow logs
+        aws_conn_id='aws_s3_conn',
+        poke_interval=60,
+        timeout=3600,
     )
 
 extract_news_info >> move_file_to_s3 >> glue_job_trigger >> grab_glue_job_run_id >> is_glue_job_finish_running
